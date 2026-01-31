575f701e711b5cd77cf0bffc19037101615c1e45
Shivani Gupta <166619882+ShivaniGupta164@users.noreply.github.com>
2026-01-31 10:46:54 +0530

    Time: 31 ms (57.49%) | Memory: 54 MB (87.56%) - LeetSync

--- 937-online-stock-span/online-stock-span.java
+++ 937-online-stock-span/online-stock-span.java
@@ -1,17 +1,21 @@
 class StockSpanner {
 
-    public StockSpanner() {
+    Stack<int[]> stack;
 
-        
+    public StockSpanner() {
+        stack = new Stack<>();
     }
-    
-    Stack<int[]> stack = new Stack<>();
+
     public int next(int price) {
-        int res = 1;
-        while (!stack.isEmpty() && stack.peek()[0] <= price)
-            res += stack.pop()[1];
-        stack.push(new int[]{price, res});
-        return res;
+        int span = 1;
+
+        while (!stack.isEmpty() && stack.peek()[0] <= price) {
+            span += stack.peek()[1];
+            stack.pop();
+        }
+
+        stack.push(new int[]{price, span});
+        return span;
     }
 }
 
