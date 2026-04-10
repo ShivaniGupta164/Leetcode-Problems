ff00bd073498cd6d934e877ea2243a0a83b9fb33
Shivani Gupta <166619882+ShivaniGupta164@users.noreply.github.com>
2026-04-10 10:46:06 +0530

    Time: 6 ms (47.91%) | Memory: 44.1 MB (91.00%) - LeetSync

---
+++ 4115-minimum-distance-between-three-equal-elements-i/minimum-distance-between-three-equal-elements-i.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int minimumDistance(int[] arr) {
+        int n=arr.length;
+        int count=0;
+        int ans=-1;
+        if(n<3)return -1;
+        
+
+       ;
+        int smallDiff=Integer.MAX_VALUE;
+        for(int i=0; i<n-2; i++){
+            for(int j=i+1; j<n-1;j++){
+                for(int k=j+1; k<n; k++){
+                    if(arr[i]==arr[j] && arr[j]==arr[k]){
+                        count=count+3;
+                        
+                        smallDiff=Math.min((Math.abs(i-j)+Math.abs(j-k)+Math.abs(k-i)),smallDiff);
+                        
+                    }
+                    
+                }
+            }
+        }
+        if(count<3)return -1;
+       
+        return smallDiff;
+         
+    }
+}
